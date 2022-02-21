10997f6132b2ed4c14befa97138e94a253d1751d
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-02-21 18:25:21 +0900

    add: using activeClassName(delete in V6)

--- src/components/Pages/Product.js
+++ src/components/Pages/Product.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>Products 1</li>
+        <li>Products 2</li>
+        <li>Products 3</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
