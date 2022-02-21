6f8c0a5e09511b1d3c40c4d6143d7def6ecbc523
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-02-21 18:41:34 +0900

    add : using params practice

--- src/components/MainHeaders.js
+++ src/components/MainHeaders.js
@@ -8,12 +8,12 @@ const MainHeaders = () => {
       <nav>
         <ul>
           <li>
-            <NavLink activeClassName={classes.active} to="/welcome">
+            <NavLink className={classes.active} to="/welcome">
               Welcome
             </NavLink>
           </li>
           <li>
-            <NavLink activeClassName={classes.active} to="/products">
+            <NavLink className={classes.active} to="/products">
               Products
             </NavLink>
           </li>
--- src/components/Pages/ProductsDetail.js
+++ src/components/Pages/ProductsDetail.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {useParams} from "react-router";
 
 const ProductsDetail = () => {
+  const params = useParams();
+
+  console.log(params);
   return (
     <section>
       <h1>ProductsDetail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
